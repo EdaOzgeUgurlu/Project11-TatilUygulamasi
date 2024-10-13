43e544cd282cda4b717a86826c232d68be168411
User <User@GLOBAL>
2024-10-14 01:46:42 +0300

    Proje dosyası ekle.

---
+++ Project11-TatilUygulaması/Program.cs
@@ -0,0 +1,114 @@
+﻿//ilk olarak tatil paketlerini yazdır
+Console.WriteLine("Hadi Tatil Planı Yapalım");
+Console.WriteLine("Bodrum paket başlangıç fiyatı= 4000 TL");
+Console.WriteLine("Marmaris paket başlangıç fiyatı= 3000 TL");
+Console.WriteLine("Çeşme paket başlangıç fiyatı= 5000 TL");
+
+int bodrum = 4000;
+int marmaris = 3000;
+int cesme = 5000;
+
+Console.WriteLine("Hangi lokasyona gitmek istersiniz? (Bodrum-Marmaris-Çeşme)");
+
+
+#region lokasyon seçimi
+//kullanıcıya gitmek istediği yeri sor
+string lokasyon = Console.ReadLine().ToLower();
+int tatilfiyat = 0;
+switch (lokasyon)
+{
+    case "bodrum":
+        tatilfiyat = 4000;
+        Console.WriteLine("Bodrum lokasyonu ile devam ediyoruz.");
+        break;
+
+    case "marmaris":
+        tatilfiyat = 3000;
+        Console.WriteLine("Marmaris lokasyonu ile devam ediyoruz.");
+        break;
+
+    case "çeşme":
+        tatilfiyat = 5000;
+        Console.WriteLine("Çeşme lokasyonu ile devam ediyoruz.");
+        break;
+
+    default:
+        Console.WriteLine("Geçerli bir lokasyon girmediniz! Lütfen geçerli bir lokasyon adı giriniz (Bodrum-Marmaris-Çeşme)");
+        break;
+}
+
+#endregion
+
+#region Kişi Sayısı Seçimi
+//Kullanıcıya kişi sayısını soruyoruz
+
+int kullanıcı = 0;
+do
+{
+    Console.Write("Kaç kişi için bir tatil planlıyoruz?: ");
+    int.TryParse(Console.ReadLine(), out kullanıcı);
+}
+while (!(kullanıcı > 0));
+
+Console.WriteLine($"{kullanıcı} kişi için tatil planlıyoruz.");
+Console.WriteLine($"{lokasyon} tercihinizde kahvaltı ve akşam yemeği fiyatlarımıza dahildir.İyi tatiller. İyi eğlenceler.");
+
+#endregion
+
+#region Ulaşım seçimi
+//Kullanıcıya ulaşım seçimini soruyoruz
+Console.WriteLine($"{lokasyon} tercihinize nasıl gitmek istersiniz?  ");
+Console.WriteLine("1 - Kara yolu (Kişi başı ulaşım tutarı gidiş-dönüş 1500 TL)");
+Console.WriteLine("2 - Hava yolu (Kişi başı ulaşım tutarı gidiş-dönüş 4000 TL)");
+Console.WriteLine("Lütfen ulaşım seçiminizi '1' yada '2' şeklinde yapınız.");
+
+byte tercih; //"1" ve "2" girişine izin vereceğimiz için byte kullanıyoruz
+int Fiyat = 0;
+
+while (true)
+{
+    tercih = byte.Parse(Console.ReadLine());
+
+    if (tercih == 1 || tercih == 2)
+    {
+        Console.WriteLine($"{tercih} seçimi ile başarılı bir seçim yaptınız. iyi yolculuklar");
+        Fiyat = (tercih == 1) ? 1500 : 4000; // tercihe göre fiyat belirleniyor.
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Geçersiz bir değer girdiniz, Lütfen ulaşım tercihinizi '1' yada '2' şeklinde yapınız.");
+    }
+
+}
+#endregion
+
+#region Sonuc
+//Tercih edilen tatil; kişi sayısı ile çarpılarak toplam fiyat belirlenir
+int toplamFiyat = kullanıcı * (Fiyat + tatilfiyat);
+Console.WriteLine($"Planladığınız tatilin toplam tutarı: {toplamFiyat} TL'dir. \r\nİyi tatiller dileriz.");
+
+#endregion
+
+#region tekrar tatil planı
+//Yeni bir tatil planı istiyor mu sorulur.
+//Ve eğer istemiyorsa döngü sonlandırılır.
+Console.WriteLine("Yeni bir tatil planlamak istiyoranız lütfen 'evet' yazın.");
+string cevap = Console.ReadLine();
+
+if (cevap.ToLower() == "hayır")
+{
+    Console.WriteLine("Tatil planlama işlemi sonlandırıldı.");
+}
+else if (cevap.ToLower() == "evet")
+{
+    Console.WriteLine("Tatil planlama işlemi başlatılıyor...");
+}
+else
+{
+    Console.WriteLine("Lütfen 'evet' veya 'hayır' olarak cevap verin.");
+}
+
+#endregion
+
+Console.ReadKey();
---
+++ README.md
@@ -0,0 +1,33 @@
+#Pratik - Yol Arkadaþý : Tatil uygulamasý
+
+3 adet lokasyonumuz var:
+
+1 - Bodrum (Paket baþlangýç fiyatý 4000 TL)
+
+2 - Marmaris (Paket baþlangýç fiyatý 3000 TL)
+
+3 - Çeþme (Paket baþlangýç fiyatý 5000 TL)
+
+Kullanýcýmýza gitmek istediði lokasyonu sormalýyýz. Bu noktada Büyük-Küçük harf duyarlýlýðýný ortadan kaldýrmalý eðer yukarýdaki 3 seçenek dýþýnda bir lokasyon girilirse bir hata olduðunu söylemeli, girebileceði lokasyon isimlerini hatýrlatmalý ve yeniden bir veri girmesini istemeliyiz.
+
+Kullanýcýnýn girdiði veriyi bir deðiþkende tutalým.
+
+Kullanýcýya kaç kiþi için tatil planlamak istediðini soralým.
+
+Kiþi sayýsýný bir deðiþkende tutalým.
+
+Ardýndan gitmek istenilen lokasyon ve o lokasyonda tatili sýrasýnda yapabilecekleri ile ilgili bir özet bilgiyi ekrana yazdýrmalýyýz.
+
+Kullanýcýya tatiline ne þekilde gitmek istediðini sorarak aþaðýdaki seçenekleri gösterelim..
+
+2 seçeneðimiz var:
+
+1 - Kara yolu ( Kiþi baþý ulaþým tutarý gidiþ-dönüþ 1500 TL )
+
+2 - Hava yolu ( Kiþi baþý ulaþým tutarý gidiþ-dönüþ 4000 TL)
+
+" Lütfen yukarýdaki seçeneklerden bir tanesini seçiniz diyerek cevabý kullanýcýdan "1" ya da "2" olarak alalým, farklý bir deðer girilmesi durumunda bir hata olduðunu söyleyerek soruyu yeniden yöneltelim.
+
+Ardýndan gidilecek lokasyon, kiþi sayýsý ve ulaþým aracý seçenekleriyle bir toplam fiyat hesaplayýp bunu kullanýcýya sunalým.
+
+Kullanýcýya baþka bir tatil planlamak isteyip istemediðini soralým, istiyorsa uygulama ilk aþamadan çalýþmaya baþlayabilir, kullanýcý istemiyorsa iyi günler dileyerek uygulamayý sonlandýrabiliriz.
